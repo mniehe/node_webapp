75a1e19d4e393ef2e4d6aa7fda0718d848b8b95e
Mark Niehe <mniehe@gmail.com>
2015-01-28 09:52:18 -0800

    Router is now using middleware

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,19 @@
+var fix_url = require(__dirname + '/middleware/fix_url.js');
+
 module.exports = function(router) {
+
+  router.use(fix_url);
 
   router.get('/', function index(req, res) {
     var data = {title: 'Your first title'};
     res.render('index', data);
   });
-  
+
   router.get('*', function(req, res){
     res.status(404);
     res.send('Page not found');
     //res.status(404).render('404_error_template', {title: "Sorry, page not found"});
-  });    
-  
+  });
+
   return router;
 }

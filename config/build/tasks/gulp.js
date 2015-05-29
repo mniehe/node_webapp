bce5ee7f4a0654758c19a5a126b2236f4132ac59
Mark Niehe <mniehe@gmail.com>
2015-05-29 14:07:30 -0700

    Fixed a bug where the html was not being transferred properly

--- config/build/tasks/gulp.js
+++ config/build/tasks/gulp.js
@@ -2,13 +2,13 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
 gulp.task('default', function(cb) {
-  runSequence('clean', 'server', 'watch', 'browserSync', cb);
+  runSequence('server', 'watch', 'browserSync', cb);
 });
 
 gulp.task('build', function() {
   runSequence(
     'clean',
     ['lint:frontend', 'lint:backend'],
-    ['build:js', 'build:images', 'build:sass', 'copy:jspm_config']
+    ['build:js', 'build:images', 'build:sass', 'copy:jspm_config', 'copy:html']
   );
 });
--- config/build/tasks/watch.js
+++ config/build/tasks/watch.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp');
 var paths = require('../paths');
 
-gulp.task('watch', ['build:js', 'build:sass', 'build:images', 'copy:jspm_config'], function() {
+gulp.task('watch', [
+    'build:js', 
+    'build:sass', 
+    'build:images', 
+    'copy:jspm_config', 
+    'copy:html'
+  ], 
+  function() {
   gulp.watch(paths.frontend.source, ['build:js:dev']);
   gulp.watch(paths.frontend.styles, ['build:sass']);
   gulp.watch(paths.frontend.images, ['build:images']);
   gulp.watch(paths.frontend.jspmConfig, ['copy:jspm_config']);
+  gulp.watch(paths.frontend.html, ['copy:html']);
 });

11e6de9d2b4d71562513332752f11646785724d0
Mark Niehe <mniehe@gmail.com>
2015-05-29 13:53:02 -0700

    Added a copy function for all html

--- config/build/tasks/copy.js
+++ config/build/tasks/copy.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var paths = require('../paths');
 var gulpCopy = require('gulp-copy');
+var changed = require('gulp-changed');
 
 gulp.task('copy:jspm_config', function () {
   return gulp.src(paths.frontend.jspmConfig)
     .pipe(gulpCopy('dist', { prefix: 1 }));
+});
+
+gulp.task('copy:html', function() {
+  return gulp.src(paths.frontend.html)
+    .pipe(changed(paths.output.source, {extension: '.html'}))
+    .pipe(gulp.dest(paths.output.source));
 });

5d9248da49d9e019bee7953584b37ba0165c093b
Mark Niehe <mniehe@gmail.com>
2015-07-25 03:22:32 -0700

    Added a default route that will serve the html file

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var koa         = require('koa'),
     validate    = require('koa-validate'),
     logger      = require('koa-logger'),
     staticFiles = require('koa-static'),
+    router      = require('koa-router')(),
+    views       = require('koa-render'),
+    underscore  = require('underscore'),
     Routes      = require('require-dir')('./backend/controllers'),
     DB          = require('./config/database');
     
@@ -17,9 +20,22 @@ if (!isTest) {
 }
 
 // Load all the default middleware
+app.use(views('./backend/views', {
+  map: {
+    html: 'underscore'
+  }
+}));
+
 app.use(betterBody());
 app.use(require('koa-validate')());
 app.use(staticFiles('./dist'));
+
+// Set the default route to load the index file
+router.get('/', function *() {
+    console.log
+    this.body = yield this.render('index.html'); 
+});
+app.use(router.routes());
 
 // Parse the middleware folder for routers and load if necessary
 for (var route in Routes) {
